f56a05c5563c7afefb66ddb64fddb4499e3f1359
Aaron Lee <jaehoaaronlee@gmail.com>
2022-04-16 22:17:15 -0700

    Add solution to #17 longest repeating characters replacement

---
+++ 017-longest-repeating-characters-replacement.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, maxLen = 0, 0
+        count = {}
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            
+            maxLen = max(maxLen, r - l + 1)
+            
+        return maxLen
